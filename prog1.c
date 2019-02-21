704bfd79bcf1709876c2ea084ed6e6f229f4ca2e
samathareddym <35551996+samathareddym@users.noreply.github.com>
2019-02-21 11:09:30 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<string.h>
+#define N strlen(g)
+void crc();
+void XOR();
+char g[20];
+char text[100];
+char code[100];
+int len;
+int main()
+{
+	int i,flag=0;
+	printf("\nenter the message in binary form\n");
+	scanf("%s",text);
+	printf("\n enter generated number\n");
+	scanf("%s",g);
+	len=strlen(text);
+	for(i=len;i<len+N-1;i++)
+	{
+		text[i]='0';
+	}
+	crc();
+	for(i=len;i<len+N-1;i++)
+	{
+		text[i]=code[i-len];
+	}
+	printf("\nfinal codeword is %s \n",text);
+	printf("\n enter the coded word");
+	scanf("%s",text);
+	crc();
+	for(i=0;i<strlen(code);i++)
+	{
+		if(code[i]=='1')
+		{
+			flag=1;
+			break;
+		}		
+	}
+	if(flag==1)
+	{
+		printf("\n error \n");
+	}
+	else
+	{
+		printf("\n no eror \n");
+	}	
+	return 0;
+}
+void crc()
+{
+	int i,j;
+	for(i=0;i<N;i++)
+	{
+		code[i]=text[i];
+	}
+	do
+	{
+		if(code[0]=='1')
+		{
+			XOR();
+		}
+		for(j=0;j<N-1;j++)
+		{
+			code[j]=code[j+1];
+		}
+		code[j]=text[i];
+		i++;
+	}while(i<=len+N-1);
+}	
+void XOR()
+{
+	int i;
+	for(i=0;i<N;i++)
+	{
+		if(code[i]==g[i])
+		{
+			code[i]='0';
+		}
+		else
+		{
+			code[i]='1';
+		}
+	}
+}
